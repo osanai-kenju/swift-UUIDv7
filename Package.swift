b366da578deaaea9a64fa72495a0baafd8ebe637
長内健樹 <osanaikenju@Am6.local>
2026-09-20 09:46:56 +0900

    fix: Package.swiftのTest対応

--- Package.swift
+++ Package.swift
@@ -18,6 +18,10 @@ let package = Package(
     targets: [
         .target(
             name: "UUIDv7"
+        ),
+        .testTarget(
+            name: "UUIDv7Tests",
+            dependencies: ["UUIDv7"]
         )
     ]
 )
